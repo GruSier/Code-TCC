02899f2f3485999a0448771ac677e9f64d65b537
GruSier <joaovreis015@gmail.com>
2020-08-14 10:44:03 -0300

    atualizacao

--- Fortran/main.f90
+++ Fortran/main.f90
@@ -2,51 +2,53 @@ program main
 	implicit none
 	character*16::filename
 	character*3::num
-	integer(8),  parameter :: Nr_max =50, Nz_max =100, Nr_min=Nr_max/9, Nz_min=Nz_max/9
+	integer(8),  parameter :: Nr_max =45, Nz_max =45, Nr_min=Nr_max/9, Nz_min=Nz_max/9
 	integer(8):: Nr,Nz,Nr_i,Nr_f,Nr_inc, nume, Nri,iexp,GCI_bol,i
 	integer(8),dimension(3):: n_gci
 	real(8):: Ta,Lr,Lz,Tinf,Erro_p_max, Erro_p_med
-	real(8),dimension(Nr_min*Nz_min,3):: T_gci
+	real(8),dimension(Nr_min*Nz_min,3):: T_gci,ErroMMS2
 	real(8),dimension(3):: h_gci
-	real(8),dimension(Nr_min*Nz_min):: Tpas,error95,Rl_pas,Zl_pas
+	real(8),dimension(Nr_min*Nz_min):: Tpas,Tfpas,error95,Rl_pas,Zl_pas,ErroMMS,p
 
 	Ta=37.d0
-	!Ta=1
+!	Ta=1
 	Tinf=20.d0
 
-	Nr_i=3
-	Nr_f=3
+	Nr_i=30
+	Nr_f=30
 	Nr_inc=1
 
 	Lr=4.d-2
 	Lz=16.d-2
 
-	GCI_bol=0
+
+	GCI_bol=1
 	IF (GCI_bol==0) THEN
 		iexp=-1
-		Nr_i=48
+		Nr_i=45
 		Nr_f=Nr_i
-		Nr_inc=5
+		Nr_inc=10
 		WRITE(unit=num,fmt='(I2.2)')Nr+1
 		filename="converg3.txt"
 		OPEN(unit=10,file=filename,status="replace")
 		do Nr=Nr_i,Nr_f,Nr_inc
 			!Nz=(Lz/Lr)*Nr
 			Nz=Nr
-			CALL diffusion(Nr,Nz,Lr,Lz,Ta,Tinf,Erro_p_max,Erro_p_med,iexp,Nr_min,Nz_min,Tpas,Rl_pas,Zl_pas)
+			CALL diffusion(Nr,Nz,Lr,Lz,Ta,Tinf,Erro_p_max,Erro_p_med,iexp,Nr_min,Nz_min,Tpas,Tfpas,Rl_pas,Zl_pas,ErroMMS)
 			PRINT *, "foi"
 			PRINT *, "Erro", Erro_p_max
 			WRITE(unit=10,fmt=*) Nr, Erro_p_max , Erro_p_med
 		end do
-		close(unit=11)
+		close(unit=10)
 
 	ELSE
 
 		do iexp=3,1,-1
 			Nr=Nr_max/(3.d0**(iexp-1.d0))
 			Nz=Nz_max/(3.d0**(iexp-1.d0))
-			CALL diffusion(Nr,Nz,Lr,Lz,Ta,Tinf,Erro_p_max,Erro_p_med,iexp,Nr_min,Nz_min,Tpas,Rl_pas,Zl_pas)
+			CALL diffusion(Nr,Nz,Lr,Lz,Ta,Tinf,Erro_p_max,Erro_p_med,iexp,Nr_min,Nz_min,Tpas,Tfpas,Rl_pas,Zl_pas,ErroMMS)
 			T_gci(:,iexp)=Tpas
+			ErroMMS2(:,iexp)=ErroMMS
 			n_gci(iexp)=Nr*Nz
 			h_gci(iexp)=(Lr*Lz/(dble(n_gci(iexp))))**0.5d0
 			print *,"Nr",Nr,"Lr",Lr,"Nz",Nz,"Lz",Lz,"dr",Lr/dble(Nr),"dz",Lz/dble(Nz)
@@ -54,19 +56,20 @@ program main
 
 		end do
 
-		CALL GCI(Nr_min, Nz_min,T_gci ,n_gci, h_gci,error95)
+		CALL GCI(Nr_min, Nz_min,T_gci ,n_gci, h_gci,error95,p)
 
 		filename="Temp_gci.txt"
 		OPEN(unit=11,file=filename,status="replace")
 		DO i=1,Nr_min*Nz_min
-			WRITE(unit=11,fmt='(f10.6,A,f10.7,A,f9.7,A,f9.7)')T_gci(i,1), ";", error95(i), ";", Rl_pas(i),";", Zl_pas(i)
+			WRITE(unit=11,fmt='(f11.6,A,f11.7,A,f11.7,A,f11.7,A,f11.7)')T_gci(i,1), ";", error95(i), ";", Rl_pas(i),";", Zl_pas(i),";", p(i)
 		END DO
 		CLOSE(unit=11)
 
-		filename="Temp_gci_sec.txt"
+		filename="Conv_MMS.txt"
 		OPEN(unit=11,file=filename,status="replace")
-		DO i=1,Nr_min*Nz_min,Nz_min
-			WRITE(unit=11,fmt='(f10.6,A,f10.7,A,f9.7,A,f9.7)')T_gci(i,1), ";", error95(i), ";", Rl_pas(i),";", Zl_pas(i)
+		WRITE(unit=11,fmt='(f15.12,A,f15.12,A,f15.12)')h_gci(1), ";", h_gci(2), ";", h_gci(3)
+		DO i=1,Nr_min*Nz_min
+			WRITE(unit=11,fmt='(f17.15,A,f17.15,A,f17.15)')abs(ErroMMS2(i,1)), ";", abs(ErroMMS2(i,2)), ";",abs(ErroMMS2(i,3))
 		END DO
 		CLOSE(unit=11)
 
@@ -76,36 +79,42 @@ program main
 
 end program main
 
-SUBROUTINE diffusion(Nr,Nz,Lr,Lz,Ta,Tinf,Erro_p_max,Erro_p_med,iexp,Nr_min,Nz_min,Tpas,Rl_pas,Zl_pas)
+SUBROUTINE diffusion(Nr,Nz,Lr,Lz,Ta,Tinf,Erro_p_max,Erro_p_med,iexp,Nr_min,Nz_min,Tpas,Tfpas,Rl_pas,Zl_pas,ErroMMS)
 	implicit none
 	character*15::filename
 	character(12) :: steste
 	integer(8)::Nr, Nz,iexp,Nr_min,Nz_min
 	integer(8)::i,j,integ, c,wr,raz_pel,raz_mus,raz_os, raz_total
 	real(8)::L,Lr,Lz,Ta,Tb,dr,dz,Cb,Rhop,Wp,h,Tinf,Ateste,Erro_p_max,Erro_p_med, Ts,T0, sum,K_pel,K_os,K_mus,W_pel, W_mus,W_os, NLin
-	real(8):: K_tumor, W_tumor
+	real(8):: K_tumor, W_tumor,Gm_tumor,Gm_pel,Gm_mus,Gm_os
 	real(8)::T3,T2,T1,R3,R2,R1,qp,qp1,qp2,qp3, qpsurface
 	real(8)::C1,C2,C3,C4,Wc, sourterm, P,P2
 	real(8),  parameter :: PI  = 4 * atan (1.0_8)
 	real(8)::AreaT, Area1,Area2, Area3
-	real(8),dimension(Nr,Nz)::R,Z,K, Erro_mat,W
+	real(8),dimension(Nr,Nz)::R,Z,K, Erro_mat,W,Gm
 	real(8),dimension(Nr*Nz,Nr*Nz)::A
-	real(8),dimension(Nr*Nz)::Kl,Rl,Zl,Wl, B
+	real(8),dimension(Nr*Nz)::Kl,Rl,Zl,Wl, B,Gml
 	real(8),dimension(Nr*Nz)::Area,T,Tn
 	real(8),dimension(Nr*Nz)::T_a,Erro,Erro_p,Tf
 	real(8)::tempo1,tempo2,tempo3, diftempo, diftempo2
-	real(8),dimension(Nr_min*Nz_min)::Tpas,Rl_pas,Zl_pas
-	real(8)::ZE,ZW,RN,RS,kls,wps,kp,T_inf,MMS,href
+	real(8),dimension(Nr_min*Nz_min)::Tpas,Tfpas,Rl_pas,Zl_pas,ErroMMS
+	real(8)::ZE,ZW,RN,RS,kls,wps,kp,T_inf,href,Gms
+	integer(8):: MMS,impTxt
+
 
 	A=0.d0
 	B=0.d0
 	R=0.d0
 	Z=0.d0
 	K=0.d0
+	Gm=0.d0
 	W=0.d0
 	T=0.d0
 
-	MMS=1.d0
+
+
+	MMS=0.d0
+	impTxt=0
 	NLin=1.d0
 
 	Rhop=1100.d0
@@ -138,8 +147,13 @@ SUBROUTINE diffusion(Nr,Nz,Lr,Lz,Ta,Tinf,Erro_p_max,Erro_p_med,iexp,Nr_min,Nz_mi
 	W_mus=7.03d-4;
 	W_os=6.57d-4;
 
-	K_tumor=0.449d0;
+	Gm_pel=1846.35d0;
+	Gm_mus=991.9d0;
+	Gm_os=541.88d0;
+
+	K_tumor=K_mus;
 	W_tumor= W_mus*10.d0;
+	Gm_tumor=	Gm_mus*10.d0;
 
 	raz_pel=1
 	raz_mus=4
@@ -150,6 +164,7 @@ SUBROUTINE diffusion(Nr,Nz,Lr,Lz,Ta,Tinf,Erro_p_max,Erro_p_med,iexp,Nr_min,Nz_mi
 		do j=1,Nz
 			K(i,j)=K_pel
 			W(i,j)=W_pel
+			Gm(i,j)=Gm_pel
 		end do
 	end do
 
@@ -157,6 +172,7 @@ SUBROUTINE diffusion(Nr,Nz,Lr,Lz,Ta,Tinf,Erro_p_max,Erro_p_med,iexp,Nr_min,Nz_mi
 		do j=1,Nz
 			K(i,j)=K_mus
 			W(i,j)=W_mus
+			Gm(i,j)=Gm_mus
 		end do
 	end do
 
@@ -164,15 +180,21 @@ SUBROUTINE diffusion(Nr,Nz,Lr,Lz,Ta,Tinf,Erro_p_max,Erro_p_med,iexp,Nr_min,Nz_mi
 		do j=1,Nz
 			K(i,j)=K_os
 			W(i,j)=W_os
+			Gm(i,j)=Gm_os
 		end do
 	end do
 
+	K=K_mus
+	W=W_mus
+	Gm=Gm_mus
+
 	print *, "dr:",dr,"dz",dz
 
-!	do i=23,27
-!		do j=25,25
-!			K(i,j)=K_tumor
-!			W(i,j)=W_tumor
+!	do i=10,20
+!		do j=20,30
+!		K(i,j)=K_tumor
+!		W(i,j)=W_tumor
+!		Gm(i,j)=Gm_tumor
 !		end do
 !	end do
 
@@ -182,8 +204,10 @@ SUBROUTINE diffusion(Nr,Nz,Lr,Lz,Ta,Tinf,Erro_p_max,Erro_p_med,iexp,Nr_min,Nz_mi
 			Zl(Nz*(i-1)+j)=Z(i,j)
 			Kl(Nz*(i-1)+j)=K(i,j)
 			Wl(Nz*(i-1)+j)=W(i,j)
+			Gml(Nz*(i-1)+j)=Gm(i,j)
 		enddo
 	enddo
+
 
 	IF (MMS==1) THEN
 		K=0.5d0;
@@ -203,8 +227,6 @@ SUBROUTINE diffusion(Nr,Nz,Lr,Lz,Ta,Tinf,Erro_p_max,Erro_p_med,iexp,Nr_min,Nz_mi
 				c=(i-1)*Nz+j
 				P2=-dsin(C3*Z(i,j)+C4)
 				Tf(Nz*(i-1)+j)=P+P2+dsin(C1*R(i,j)+C2)*dsin(C3*Z(i,j)+C4)
-				!print*, dcos(C1*Lr+C2),"ok"
-				!read(*,*)
 			enddo
 		enddo
 		filename="Temp_f.txt"
@@ -215,7 +237,7 @@ SUBROUTINE diffusion(Nr,Nz,Lr,Lz,Ta,Tinf,Erro_p_max,Erro_p_med,iexp,Nr_min,Nz_mi
 		CLOSE(unit=11)
 	END IF
 
-	CALL fvm(Nr,Nz,Lr,Lz,Ta,dr,dz,Cb,Rhop,h,Tinf, NLin,A,Kl,Rl,Zl,Wl,B)
+	CALL fvm(Nr,Nz,Lr,Lz,Ta,dr,dz,Cb,Rhop,h,Tinf, NLin,A,Kl,Rl,Zl,Wl,Gml,B)
 
 		kp=K_pel
 		T_inf=Tinf
@@ -225,6 +247,7 @@ SUBROUTINE diffusion(Nr,Nz,Lr,Lz,Ta,Tinf,Erro_p_max,Erro_p_med,iexp,Nr_min,Nz_mi
 				do j=1,Nz
 					c=(i-1)*Nz+j
 					kls=Kl(c)
+					Gms=Gml(c)
 					ZE=Z(i,j)+dz/2.d0
 					ZW=Z(i,j)-dz/2.d0
 					RN=R(i,j)+dr/2.d0
@@ -235,6 +258,7 @@ SUBROUTINE diffusion(Nr,Nz,Lr,Lz,Ta,Tinf,Erro_p_max,Erro_p_med,iexp,Nr_min,Nz_mi
  - 1.0d0/2.0d0*C3*RN**2*kls*cos(C3*ZE + C4) + (1.0d0/2.0d0)*C3*RS**2*kls*cos(C3*ZE + C4) &
  - 1.0d0/2.0d0*Cb*RN**2*Rhop*T_inf*wps*ZE + (1.0d0/2.0d0)*Cb*RN**2*Rhop*Ta*wps*ZE &
  + (1.0d0/2.0d0)*Cb*RS**2*Rhop*T_inf*wps*ZE - 1.0d0/2.0d0*Cb*RS**2*Rhop*Ta*wps*ZE &
+ - 1.0d0/2.0d0*(Gms*RN**2)*ZE + (1.0d0/2.0d0)*Gms*(RS**2)*ZE+&
  - 1.0d0/2.0d0*Cb*RN**2*Rhop*wps*cos(C3*ZE + C4)/C3 +(1.0d0/2.0d0)*Cb*RS**2*Rhop*wps*cos(C3*ZE + C4)/C3 &
  - C3*RN*kls*cos(C1*RN + C2)*cos(C3*ZE + C4)/C1 + C3*RS*kls*cos(C1*RS + C2)*cos(C3*ZE + C4)/C1 &
  - Cb*RN*Rhop*wps*cos(C1*RN + C2)*cos(C3*ZE + C4)/(C1*C3) + Cb*RS*Rhop*wps*cos(C1*RS + C2)*cos(C3*ZE + C4)/(C1*C3) &
@@ -244,6 +268,7 @@ SUBROUTINE diffusion(Nr,Nz,Lr,Lz,Ta,Tinf,Erro_p_max,Erro_p_med,iexp,Nr_min,Nz_mi
  + (1.0d0/2.0d0)*C3*RN**2*kls*cos(C3*ZW +C4) - 1.0d0/2.0d0*C3*RS**2*kls*cos(C3*ZW + C4) &
  + (1.0d0/2.0d0)*Cb*RN**2*Rhop*T_inf*wps*ZW - 1.0d0/2.0d0*Cb*RN**2*Rhop*Ta*wps*ZW &
  - 1.0d0/2.0d0*Cb*RS**2*Rhop*T_inf*wps*ZW + (1.0d0/2.0d0)*Cb*RS**2*Rhop*Ta*wps*ZW &
++ 1.0d0/2.0d0*(Gms*RN**2)*ZE - (1.0d0/2.0d0)*Gms*(RS**2)*ZW+&
  + (1.0d0/2.0d0)*Cb*RN**2*Rhop*wps*cos(C3*ZW + C4)/C3- 1.0d0/2.0d0*Cb*RS**2*Rhop*wps*cos(C3*ZW + C4)/C3 &
  + C3*RN*kls*cos(C1*RN + C2)*cos(C3*ZW + C4)/C1 - C3*RS*kls*cos(C1*RS + C2)*cos(C3*ZW + C4)/C1 &
  + Cb*RN*Rhop*wps*cos(C1*RN + C2)*cos(C3*ZW + C4)/(C1*C3) - Cb*RS*Rhop*wps*cos(C1*RS + C2)*cos(C3*ZW + C4)/(C1*C3)&
@@ -257,11 +282,11 @@ SUBROUTINE diffusion(Nr,Nz,Lr,Lz,Ta,Tinf,Erro_p_max,Erro_p_med,iexp,Nr_min,Nz_mi
 		END IF
 
 		tempo2=time()
-		CALL sor(T,A,B,Nr*Nz)
+
+		CALL sor(T,A,B,Nr,Nz,dr,dz)
+
 		tempo3=time()
-		diftempo=tempo2-tempo1
-		diftempo2=tempo3-tempo2
-		PRINT * , "Tempo:", diftempo2
+		PRINT * , "Tempo:", tempo3-tempo2
 
 		IF (iexp==-1)  THEN
 			IF (NLin==0.d0) THEN
@@ -287,30 +312,34 @@ SUBROUTINE diffusion(Nr,Nz,Lr,Lz,Ta,Tinf,Erro_p_max,Erro_p_med,iexp,Nr_min,Nz_mi
 			Tpas=0
 		ELSE IF ( iexp==3 ) THEN
 			Tpas=T
+			Tfpas=Tf
 			Rl_pas=Rl
 			Zl_pas=Zl
 		ELSE IF ( iexp==2 ) THEN
 			DO i=1,Nr_min
 				DO j=1,Nz_min
 					Tpas((i-1)*Nr_min+j)=T(3*3*Nz_min*(i-1)+3*Nz_min+2+(j-1)*3)
-					!print*, (i-1)*Nr_min+j , "->",3*3*Nr_min*(i-1)+3*Nz_min+2+(j-1)*3
+					Tfpas((i-1)*Nr_min+j)=Tf(3*3*Nz_min*(i-1)+3*Nz_min+2+(j-1)*3)
 				END DO
 			END DO
 		ELSE
 			DO i=1,Nr_min
 				DO j=1,Nz_min
 					Tpas((i-1)*Nr_min+j)=T(9*9*Nz_min*(i-1)+9*Nz_min*4+5+(j-1)*9)
-					!print*, (i-1)*Nr_min+j , "->",9*9*Nz_min*(i-1)+9*Nz_min*4+5+(j-1)*9
+					Tfpas((i-1)*Nr_min+j)=Tf(9*9*Nz_min*(i-1)+9*Nz_min*4+5+(j-1)*9)
 				END DO
 			END DO
 		END IF
+
+
+		ErroMMS=Tpas-Tfpas
 
 		PRINT *, "erro:",DABS(MAXVAL(T-Tf)),"href",href
 
 		filename="Temp_a.txt"
 		OPEN(unit=11,file=filename,status="replace")
 		DO i=20,Nr*Nz,Nz
-			WRITE(unit=11,fmt='(f12.6,A,f12.6,A,f11.6,A,f11.6)')T(i), ";", T_a(i), ";", Erro_p(i), ";" , Rl(i)
+			WRITE(unit=11,fmt='(f12.6,A,f12.6,A,f11.6,A,f11.6,A,f11.6)')T(i), ";", T_a(i), ";", Erro_p(i), ";" , Rl(i), ";" , Zl(i)
 		END DO
 		CLOSE(unit=11)
 
@@ -352,13 +381,13 @@ SUBROUTINE diffusion(Nr,Nz,Lr,Lz,Ta,Tinf,Erro_p_max,Erro_p_med,iexp,Nr_min,Nz_mi
 
 END SUBROUTINE diffusion
 
-SUBROUTINE fvm(Nr,Nz,Lr,Lz,Ta,dr,dz,Cb,Rhop,h,Tinf, NLin,A,Kl,Rl,Zl,Wl,B)
+SUBROUTINE fvm(Nr,Nz,Lr,Lz,Ta,dr,dz,Cb,Rhop,h,Tinf, NLin,A,Kl,Rl,Zl,Wl,Gml,B)
 	implicit none
 	integer(8)::Nr, Nz
 	integer(8)::i,j, c
 	real(8)::Lr,Lz,Ta,dr,dz,Cb,Rhop,h,Tinf, NLin
 	real(8),dimension(Nr*Nz,Nr*Nz)::A
-	real(8),dimension(Nr*Nz)::Kl,Rl,Zl,Wl, B
+	real(8),dimension(Nr*Nz)::Kl,Rl,Zl,Wl,Gml, B
 
 		do i=2,Nr-1            !Looping dos elementos internos
 			do j=2,Nz-1
@@ -368,7 +397,7 @@ SUBROUTINE fvm(Nr,Nz,Lr,Lz,Ta,dr,dz,Cb,Rhop,h,Tinf, NLin,A,Kl,Rl,Zl,Wl,B)
 				A(c,c+1)=(2.d0/(1.d0/Kl(c)+1.d0/Kl(c+1)))*Rl(c)*dr/dz !Eact
 				A(c,c-1)=(2.d0/(1.d0/Kl(c)+1.d0/Kl(c-1)))*Rl(c)*dr/dz !West
 				A(c,c)=-1.d0*(A(c,c-Nz)+A(c,c+Nz)+A(c,c+1)+A(c,c-1)+Rhop*cb*Wl(c)*Rl(c)*dr*dz*NLin)
-				B(c) = -Rhop*cb*Wl(c)*Rl(c)*dr*dz*Ta
+				B(c) = -Rhop*cb*Wl(c)*Rl(c)*dr*dz*Ta-Gml(c)*Rl(c)*dr*dz
 			enddo
 		enddo
 
@@ -385,7 +414,7 @@ SUBROUTINE fvm(Nr,Nz,Lr,Lz,Ta,dr,dz,Cb,Rhop,h,Tinf, NLin,A,Kl,Rl,Zl,Wl,B)
 					A(c,c+1)=(2.d0/(1.d0/Kl(c)+1.d0/Kl(c+1)))*Rl(c)*dr/dz !East
 					A(c,c-1)=0.d0                                         !West
 					A(c,c)=-1.d0*(A(c,c-Nz)+A(c,c+Nz)+A(c,c+1)+Rhop*Cb*Wl(c)*Rl(c)*dr*dz*NLin)
-					B(c) = -Rhop*Cb*Wl(c)*Rl(c)*dr*dz*Ta
+					B(c) = -Rhop*Cb*Wl(c)*Rl(c)*dr*dz*Ta-Gml(c)*Rl(c)*dr*dz
 				enddo
 			enddo
 
@@ -398,7 +427,7 @@ SUBROUTINE fvm(Nr,Nz,Lr,Lz,Ta,dr,dz,Cb,Rhop,h,Tinf, NLin,A,Kl,Rl,Zl,Wl,B)
 					A(c,c+1)=0.d0                                         !East
 					A(c,c-1)=(2.d0/(1.d0/Kl(c)+1.d0/Kl(c-1)))*Rl(c)*dr/dz !West
 					A(c,c)=-1.d0*(A(c,c-Nz)+A(c,c+Nz)+A(c,c-1)+Rhop*Cb*Wl(c)*Rl(c)*dr*dz*NLin)
-					B(c) = -Rhop*Cb*Wl(c)*Rl(c)*dr*dz*Ta
+					B(c) = -Rhop*Cb*Wl(c)*Rl(c)*dr*dz*Ta-Gml(c)*Rl(c)*dr*dz
 				enddo
 			enddo
 
@@ -410,7 +439,7 @@ SUBROUTINE fvm(Nr,Nz,Lr,Lz,Ta,dr,dz,Cb,Rhop,h,Tinf, NLin,A,Kl,Rl,Zl,Wl,B)
 					A(c,c+1)=(2.d0/(1.d0/Kl(c)+1.d0/Kl(c+1)))*Rl(c)*dr/dz !East
 					A(c,c-1)=(2.d0/(1.d0/Kl(c)+1.d0/Kl(c-1)))*Rl(c)*dr/dz !West
 					A(c,c)=-1.d0*(A(c,c-Nz)+A(c,c+1)+A(c,c-1)+Rhop*Cb*Wl(c)*Rl(c)*dr*dz*NLin)
-					B(c) = -Rhop*cb*Wl(c)*Rl(c)*dr*dz*Ta
+					B(c) = -Rhop*cb*Wl(c)*Rl(c)*dr*dz*Ta-Gml(c)*Rl(c)*dr*dz
 				enddo
 			enddo
 
@@ -422,7 +451,7 @@ SUBROUTINE fvm(Nr,Nz,Lr,Lz,Ta,dr,dz,Cb,Rhop,h,Tinf, NLin,A,Kl,Rl,Zl,Wl,B)
 					A(c,c+1)=(2.d0/(1.d0/Kl(c)+1.d0/Kl(c+1)))*Rl(c)*dr/dz !Eact
 					A(c,c-1)=(2.d0/(1.d0/Kl(c)+1.d0/Kl(c-1)))*Rl(c)*dr/dz !West
 					A(c,c)=-1.d0*(A(c,c+Nz)+A(c,c+1)+A(c,c-1)+Rhop*Cb*Wl(c)*Rl(c)*dr*dz*NLin+h/(1.d0+h*dr/Kl(c))*(Rl(c)+dr/2.d0)*dz)
-					B(c) = -Rhop*Cb*Wl(c)*Rl(c)*dr*dz*Ta-h/(1.d0+h*dr/Kl(c))*(Rl(c)+dr/2.d0)*dz*Tinf
+					B(c) = -Rhop*Cb*Wl(c)*Rl(c)*dr*dz*Ta-h/(1.d0+h*dr/Kl(c))*(Rl(c)+dr/2.d0)*dz*Tinf-Gml(c)*Rl(c)*dr*dz
 				enddo
 			enddo
 
@@ -433,7 +462,7 @@ SUBROUTINE fvm(Nr,Nz,Lr,Lz,Ta,dr,dz,Cb,Rhop,h,Tinf, NLin,A,Kl,Rl,Zl,Wl,B)
 			A(c,c+Nz)=(2.d0/(1.d0/Kl(c)+1.d0/Kl(c+Nz)))*(Rl(c)+Rl(c+Nz))/2.d0*dz/dr !South
 			A(c,c+1)=(2.d0/(1.d0/Kl(c)+1.d0/Kl(c+1)))*Rl(c)*dr/dz !East
 			A(c,c)=-1.d0*(A(c,c+Nz)+A(c,c+1)+Rhop*Cb*Wl(c)*Rl(c)*dr*dz*NLin+h/(1.d0+h*dr/Kl(c))*(Rl(c)+dr/2.d0)*dz)
-			B(c) = -Rhop*Cb*Wl(c)*Rl(c)*dr*dz*Ta-h/(1+h*dr/Kl(c))*(Rl(c)+dr/2.d0)*dz*Tinf
+			B(c) = -Rhop*Cb*Wl(c)*Rl(c)*dr*dz*Ta-h/(1+h*dr/Kl(c))*(Rl(c)+dr/2.d0)*dz*Tinf-Gml(c)*Rl(c)*dr*dz
 
 			!Canto N-E!
 			i=1
@@ -442,7 +471,7 @@ SUBROUTINE fvm(Nr,Nz,Lr,Lz,Ta,dr,dz,Cb,Rhop,h,Tinf, NLin,A,Kl,Rl,Zl,Wl,B)
 			A(c,c+Nz)=(2.d0/(1.d0/Kl(c)+1.d0/Kl(c+Nz)))*(Rl(c)+Rl(c+Nz))/2.d0*dz/dr !South
 			A(c,c-1)=(2.d0/(1.d0/Kl(c)+1.d0/Kl(c-1)))*Rl(c)*dr/dz !West
 			A(c,c)=-1.d0*(A(c,c+Nz)+A(c,c-1)+Rhop*Cb*Wl(c)*Rl(c)*dr*dz*NLin+h/(1.d0+h*dr/Kl(c))*(Rl(c)+dr/2.d0)*dz)
-			B(c) = -Rhop*Cb*Wl(c)*Rl(c)*dr*dz*Ta-h/(1+h*dr/Kl(c))*(Rl(c)+dr/2.d0)*dz*Tinf
+			B(c) = -Rhop*Cb*Wl(c)*Rl(c)*dr*dz*Ta-h/(1+h*dr/Kl(c))*(Rl(c)+dr/2.d0)*dz*Tinf-Gml(c)*Rl(c)*dr*dz
 
 			!Canto S-W!
 			i=Nr
@@ -451,7 +480,7 @@ SUBROUTINE fvm(Nr,Nz,Lr,Lz,Ta,dr,dz,Cb,Rhop,h,Tinf, NLin,A,Kl,Rl,Zl,Wl,B)
 			A(c,c-Nz)=(2.d0/(1.d0/Kl(c)+1.d0/Kl(c-Nz)))*(Rl(c)+Rl(c-Nz))/2.d0*dz/dr !Nzorth
 			A(c,c+1)=(2.d0/(1.d0/Kl(c)+1.d0/Kl(c+1)))*Rl(c)*dr/dz !East
 			A(c,c)=-1.d0*(A(c,c-Nz)+A(c,c+1)+Rhop*Cb*Wl(c)*Rl(c)*dr*dz*NLin)
-			B(c) =- Rhop*Cb*Wl(c)*Rl(c)*dr*dz*Ta
+			B(c) =- Rhop*Cb*Wl(c)*Rl(c)*dr*dz*Ta-Gml(c)*Rl(c)*dr*dz
 
 			!Canto S-E!
 			i=Nr
@@ -460,7 +489,7 @@ SUBROUTINE fvm(Nr,Nz,Lr,Lz,Ta,dr,dz,Cb,Rhop,h,Tinf, NLin,A,Kl,Rl,Zl,Wl,B)
 			A(c,c-Nz)=(2.d0/(1.d0/Kl(c)+1.d0/Kl(c-Nz)))*(Rl(c)+Rl(c-Nz))/2.d0*dz/dr!   Nzorth
 			A(c,c-1)=(2.d0/(1.d0/Kl(c)+1.d0/Kl(c-1)))*Rl(c)*dr/dz                   !West
 			A(c,c)=-1.d0*(A(c,c-Nz)+A(c,c-1)+Rhop*Cb*Wl(c)*Rl(c)*dr*dz*NLin)
-			B(c) = -Rhop*Cb*Wl(c)*Rl(c)*dr*dz*Ta
+			B(c) = -Rhop*Cb*Wl(c)*Rl(c)*dr*dz*Ta-Gml(c)*Rl(c)*dr*dz
 
 END SUBROUTINE fvm
 
@@ -518,40 +547,43 @@ SUBROUTINE analitico(Nr,Nz,Lr,raz_pel,raz_mus,raz_total,Rhop,Cb,h,W_pel,W_mus,W_
 
 END SUBROUTINE analitico
 
-SUBROUTINE sor(vx,A,vb,N)
+SUBROUTINE sor(vx,A,vb,Nr,Nz,dr,dz)
 	IMPLICIT NONE
-	INTEGER(8)::i,j,N
+	INTEGER(8)::i,j,Nr,Nz
 	REAL(8), PARAMETER::pi=4.D0*datan(1.D0)
-	REAL(8),DIMENSION(N)::vb,vx,vxnew
-	REAL(8),DIMENSION(N,N)::A
+	REAL(8),DIMENSION(Nr*Nz)::vb,vx,vxnew
+	REAL(8),DIMENSION(Nr*Nz,Nr*Nz)::A
 	INTEGER(8)::contador
-	REAL(8)::tol,eps,wopt,h
+	REAL(8)::tol,eps,wopt,h,rjac,dr,dz
 
-	h=1.d0/(dble(N)+1.d0)
-	wopt=2.d0/(1.d0+dsin(pi*h))
+	rjac=(dcos(PI/Nr)+((dr/dz)**2)*dcos(PI/Nz))/(1+(dr/dz)**2)
+	wopt=2.d0/(1.d0+(1.d0-rjac**2.d0))
 	print *, "wopt:",wopt
-	!print *, wopt
+	!h=1.d0/(dble(Nr*Nz)+1.d0)
+	!wopt=2.d0/(1.d0+dsin(pi*h))
+	print *, "wopt:",wopt
 	wopt=wopt*0.95d0
-	!wopt=1.877d0*1.01
-	!print *, wopt
+	print *, wopt
 	vxnew=0.d0
 	vx=0.d0
-	tol=1.d-3
+	tol=1.d-5
 	eps=1.d0
 	contador=0
 
 	DO WHILE(eps.gt.tol)
-		DO i = 1,N        !Varia a linha da matriz
+		DO i = 1,Nr*Nz        !Varia a linha da matriz
 			vxnew(i)=vb(i)
 			DO j = 1,i-1
 				vxnew(i)=vxnew(i)-A(i,j)*vxnew(j)
 			ENDDO
-			DO j = i+1, N
+			DO j = i+1, Nr*Nz
 				vxnew(i)=vxnew(i)-A(i,j)*vx(j)
 			ENDDO
 			vxnew(i)=wopt*vxnew(i)/A(i,i)+(1.d0-wopt)*vx(i)
 		ENDDO
+
 		eps=DABS(MAXVAL(vxnew-vx))
+		!print*,"eps:",eps
 		vx=vxnew
 		contador=contador+1
 	ENDDO
@@ -559,7 +591,94 @@ SUBROUTINE sor(vx,A,vb,N)
 
 END SUBROUTINE sor
 
-SUBROUTINE GCI(Nr_min, Nz_min,T_gci ,n_gci, h_gci,error95)
+SUBROUTINE sorc(vx,A,vb,Nr,Nz,dr,dz)
+	IMPLICIT NONE
+	INTEGER(8)::i,j,Nr,Nz,isave
+	REAL(8), PARAMETER::pi=4.D0*datan(1.D0)
+	REAL(8),DIMENSION(Nr*Nz)::vb,vx,vxnew
+	REAL(8),DIMENSION(Nr*Nz,Nr*Nz)::A
+	INTEGER(8)::contador,pass
+	REAL(8)::tol,eps,wopt,h,rjac,dr,dz
+
+	rjac=(dcos(PI/Nr)+((dr/dz)**2)*dcos(PI/Nz))/(1+(dr/dz)**2)
+	wopt=2.d0/(1.d0+(1.d0-rjac**2.d0))
+	print *, "wopt:",wopt
+	h=1.d0/(dble(Nr*Nz)+1.d0)
+	wopt=2.d0/(1.d0+dsin(pi*h))
+	!wopt=wopt*0.95d0
+	!wopt=1.877d0*1.01
+	!print *, wopt
+	wopt=1
+	vxnew=0.d0
+	vx=0.d0
+	tol=1.d-3
+	eps=1.d0
+	contador=0
+	wopt=1
+
+
+	DO WHILE(eps.gt.tol)
+			DO i = 1,Nr*Nz-1,2        !Varia a linha da matriz
+				vxnew(i)=vb(i)
+				DO j = 1,i-1,2
+					!print *,"i",i,"j",j,"pass",1,"new"
+					vxnew(i)=vxnew(i)-A(i,j)*vxnew(j)
+				ENDDO
+				DO j = i+2, Nr*Nz-1,2
+					!print *,"i",i,"j",j,"pass",1,"old"
+					vxnew(i)=vxnew(i)-A(i,j)*vx(j)
+				ENDDO
+
+				DO j = 1+1,Nr*Nz,2
+					!print *,"i",i,"j",j,"pass",1,"old"
+					vxnew(i)=vxnew(i)-A(i,j)*vx(j)
+				ENDDO
+
+				vxnew(i)=wopt*vxnew(i)/A(i,i)+(1.d0-wopt)*vx(i)
+
+			ENDDO
+
+			IF (contador==0) THEN
+				wopt=1.d0/(1.d0-(rjac**2.d0)/2.d0)
+			ELSE
+				wopt=1.d0/(1.d0-(rjac**2.d0)*wopt/4.d0)
+			ENDIF
+
+			DO i = 2,Nr*Nz,2        !Varia a linha da matriz
+				vxnew(i)=vb(i)
+				DO j = 2,i-1,2
+					!print *,"i",i,"j",j,"pass",2,"new"
+					vxnew(i)=vxnew(i)-A(i,j)*vxnew(j)
+				ENDDO
+				DO j = i+2, Nr*Nz,2
+				!!	print *,"i",i,"j",j,"pass",2,"old"
+					vxnew(i)=vxnew(i)-A(i,j)*vx(j)
+				ENDDO
+
+				DO j = 1,Nr*Nz-1,2
+					!print *,"i",i,"j",j,"pass",2,"new"
+					vxnew(i)=vxnew(i)-A(i,j)*vxnew(j)
+				ENDDO
+
+			vxnew(i)=wopt*vxnew(i)/A(i,i)+(1.d0-wopt)*vx(i)
+			ENDDO
+
+
+			wopt=1.d0/(1.d0-(rjac**2.d0)*wopt/4.d0)
+
+		eps=DABS(MAXVAL(vxnew-vx))
+		!print*,"eps:",eps
+		vx=vxnew
+		contador=contador+1
+
+
+	ENDDO
+	print*, "contador:", contador
+
+END SUBROUTINE sorc
+
+
+SUBROUTINE GCI(Nr_min, Nz_min,T_gci ,n_gci, h_gci,error95,p)
 	integer(8):: Nr_min, Nz_min ,i
 	integer(8),dimension(3):: n_gci
 	real(8),dimension(Nr_min*Nz_min,3):: T_gci
@@ -577,6 +696,6 @@ SUBROUTINE GCI(Nr_min, Nz_min,T_gci ,n_gci, h_gci,error95)
 	DO i=1,Nr_min*Nz_min
 		p(i)=(1/log(ref(1)))*(log(abs((T_gci(i,3)-T_gci(i,2))/(T_gci(i,2)-T_gci(i,1)))))
 		ea(i)=abs(T_gci(i,1)-T_gci(i,2))
-		error95(i)=FS*ea(i)/(ref(1)-1.d0)
+		error95(i)=FS*ea(i)/((ref(1)**p(i))-1.d0)
 	END DO
 END SUBROUTINE
--- Fortran/run.sh
+++ Fortran/run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 #Para permitir a execução do script: chmod +x ./run.sh
 
-gfortran $1.f90 -c
+gfortran $1.f90 -c 
 gfortran $1.o -o $1
 ./$1
